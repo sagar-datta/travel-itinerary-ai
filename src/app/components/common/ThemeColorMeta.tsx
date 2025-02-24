04c220aa15eb7f05af3e941a51122535c0179b3e
sagar-datta <s@gar.jp.net>
2025-02-24 15:04:19 +0800

    update theme color in ThemeColorMeta component for improved light mode appearance

--- src/app/components/common/ThemeColorMeta.tsx
+++ src/app/components/common/ThemeColorMeta.tsx
@@ -3,7 +3,7 @@ import { useTheme } from "@/app/context/theme/ThemeContext";
 
 export function ThemeColorMeta() {
   const { theme } = useTheme();
-  const themeColor = theme === "dark" ? "#363636" : "#E8F0F7";
+  const themeColor = theme === "dark" ? "#363636" : "#F0F0F0";
 
   return (
     <meta
